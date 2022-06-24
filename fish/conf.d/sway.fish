95f645dff90f7a00e180faa87a17cb46df35ca6f
Sean Collings <seanrcollings@gmail.com>
2022-06-24 15:37:41 -0600

    waybar updates

--- fish/conf.d/sway.fish
+++ fish/conf.d/sway.fish
@@ -3,5 +3,6 @@ if status --is-login
     and  test (tty) = "/dev/tty1" 
     # Some Java GUI applications fail without this
     set -gx _JAVA_AWT_WM_NONREPARENTING 1
+    set -gx MOZ_ENABLE_WAYLAND 1
     sway
 end
